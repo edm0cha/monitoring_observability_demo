a8183e687002001c190531b0ef190a1de58d021c
Edwin Moedano <edwin.moedano@caylent.com>
2024-10-06 17:59:55 -0600

    feat: change default name for backend terraform configuration

--- provision/init.tf
+++ provision/init.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    bucket         = "edwin-tf-state"
+    bucket         = "your-bucket-tf-state"
+    dynamodb_table = "your-table-tf-state"
     key            = "terraform-observability.tfstate"
-    dynamodb_table = "edwin-tf-state"
     encrypt        = true
   }
 }
